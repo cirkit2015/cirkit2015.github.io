3e74c79df82c0e42538ec49c4c2e8fc5176b76b7
jviola17 <jlvsoccer7@gmail.com>
2015-09-19 22:21:33 -0700

    Update cirkit.js

--- js/cirkit.js
+++ js/cirkit.js
@@ -1,1 +1,37 @@
+function allowDrop(event){
+  event.preventDefault();
+}
 
+
+function dragPowerSource(event){
+  event.dataTransfer.setData("powerSource", event.target.id);
+}
+
+function dropPowerSource(event){
+  event.preventDefault();
+  var data = event.dataTransfer.getData("powerSource");
+  event.target.appendChild(document.getElementById(data));
+  
+}
+
+function dragUSB(event){
+  event.dataTransfer.setData("USB", event.target.id);
+}
+
+function dropUSB(event){
+  event.preventDefault();
+  var data = event.dataTransfer.getData("USB");
+  event.target.appendChild(document.getElementById(data));
+  
+}
+
+function dragPhoton(event){
+  event.dataTransfer.setData("photon", event.target.id);
+}
+
+function dropPhoton(event){
+  event.preventDefault();
+  var data = event.dataTransfer.getData("photon");
+  event.target.appendChild(document.getElementById(data));
+  
+}
